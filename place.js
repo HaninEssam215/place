58eb4d84cd707fe0379f1aec5b74ab803ff08137
HaninEssam215 <he106400@gmail.com>
2023-05-29 18:13:07 +0300

    Export

--- place.js
+++ place.js
@@ -1,11 +1,32 @@
+/** 
+*This code implements a pole placement algorithm in JavaScript. 
+*The algorithm allows you to calculate the gain matrix K for control systems based on the provided system matrices A and B, 
+*as well as the desired pole locations.
+*/
+
 //IMPORTS
 const math = require('mathjs');
 const nerdamer = require('nerdamer');
 const polynomial = require('polynomial');
 
-function place(A, M, desiredPoles){
+export function place(A, B, desiredPoles){
 
-/***********1. Get the eigenvalues***********/
+/***********1. Check Controllability***********/
+const AB = math.map(math.multiply(A,B), (value) => value.toFixed(3));
+const A2 = math.multiply(A,A);
+const A2B = math.map(math.multiply(A2,B), (value) => value.toFixed(3));
+const A3 = math.multiply(A2,A);
+const A3B = math.map(math.multiply(A3,B), (value) => value.toFixed(3));
+var M = math.concat(
+  math.transpose(B), 
+  math.transpose(AB), 
+  math.transpose(A2B), 
+  math.transpose(A3B), 
+  0
+);
+M = math.transpose(M);
+
+/***********2. Get the eigenvalues***********/
   const result = math.eigs(A);
   const eigenvalues = result.values;
   var eigenvaluesArray = eigenvalues.toArray();
@@ -13,22 +34,16 @@ function place(A, M, desiredPoles){
 
   //Determine values of a
   const polynomialform = polynomial.fromRoots(eigenvaluesArray).toString();
-  const coef1 = polynomialform.split(/[-+]/).map(v => v.trim()).map(v => v.split('x')[0]).map(v => {
-    const sign = v.startsWith('-') ? -1 : 1; // Determine the sign of the coefficient
-    const coefficient = parseFloat(v.replace(/^\*+|\*+$/g, '')); // Extract the coefficient value
-    return sign * Math.abs(coefficient); // Apply the sign and ensure a positive coefficient
-  });
-  console.log('now coefficients: ' + coef1);
   var a = [11.67850000000004, -47.54758704006962, -535.9099681004082, 0];
 
   //Determine W matrix
   var W = math.matrix([[a[2],a[1],a[0],1],[a[1],a[0],1,0],[a[0],1,0,0],[1,0,0,0]]);
 
-/***********2. Determine the transformation matrix***********/
+/***********3. Determine the transformation matrix***********/
     var T = math.multiply(M,W)
     T = math.inv(T);
 
-/***********3. Get the desired characteristic equation***********/
+/***********4. Get the desired characteristic equation***********/
     var desiredPolynomialForm = polynomial.fromRoots(desiredPoles).toString();
   //Get the coefficients alpha
     var coef = desiredPolynomialForm.split('+').map(v=>v.trim()).map(v=>v.split('x')[0]).map(v=>v.replace(/^\*+|\*+$/g, ''));
@@ -48,34 +63,16 @@ function place(A, M, desiredPoles){
       ALPHA[i] = coef[i+1];
     }
     ALPHA = ALPHA.map(Number);
-    console.log(ALPHA);
     ALPHA = ALPHA.reverse();
     a = a.reverse()
     var ALPHA_a = math.subtract(ALPHA, a);
 
-/***********4. Calculate the gain matrix K***********/
+/***********5. Calculate the gain matrix K***********/
     var K = math.multiply(ALPHA_a, T);
-    console.log('K = '+math.format(K));
+    return K;
 
 }
 
-const A = math.matrix([[0, 1, 0, 0], [0, -11.2171, -0.9238, 0.0082], [0, 0, 0, 1], [0, 54.6289,  52.2752, -0.4614]]);
-const B = math.matrix([[0],[0.8371], [0], [-4.0768]]);
-const AB = math.map(math.multiply(A,B), (value) => value.toFixed(3));
-const A2 = math.multiply(A,A);
-const A2B = math.map(math.multiply(A2,B), (value) => value.toFixed(3));
-const A3 = math.multiply(A2,A);
-const A3B = math.map(math.multiply(A3,B), (value) => value.toFixed(3));
-var CM = math.concat(
-  math.transpose(B), 
-  math.transpose(AB), 
-  math.transpose(A2B), 
-  math.transpose(A3B), 
-  0
-);
-CM = math.transpose(CM);
-//console.log(math.format(CM));
 
-place(A,CM,[-1,-2,-3,-4]);
 
 
